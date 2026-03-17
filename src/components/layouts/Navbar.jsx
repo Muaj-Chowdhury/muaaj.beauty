9eeaf71484cc715b3539752a6ec6996e50896e3b
Muaj Chowdhury <muaaj24@gmail.com>
2026-03-18 00:05:24 +0600

    category and  concern based query

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     hasDropdown: true,
     categories: [
       { name: 'Skincare', href: '/shop/category/skincare' },
-      { name: 'Cosmetics', href: '/shop/category/personal-care' },
+      { name: 'Cosmetics', href: '/shop/category/cosmetics' },
       { name: 'Haircare', href: '/shop/category/haircare' },
     ],
     concerns: [
